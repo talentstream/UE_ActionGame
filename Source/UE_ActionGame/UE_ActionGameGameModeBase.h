e098797b8f0dc31d44f238925a1fa86e1de11495
talentstream <447721325@qq.com>
2023-05-18 23:50:05 +0800

    Initial
    Initial

---
+++ Source/UE_ActionGame/UE_ActionGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_ActionGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_ACTIONGAME_API AUE_ActionGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
